9e6ffec9b25e362de88fc2365c181221e8e0e9fb
Valentin <sdms@abv.bg>
2020-06-15 18:40:57 +0300

    Some Required tags for bonus security

--- HireManagement/HireManagement/Models/Contract.cs
+++ HireManagement/HireManagement/Models/Contract.cs
@@ -10,15 +10,22 @@ namespace HireManagement.Models
     public class Contract
     {
         public int ContractID { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string ContractName { get; set; }
+        [Required]
         public DateTime ExpiryDate { get; set; }
+        [Required]
+        [MaxLength(10)]
         public double Reward { get; set; }
+        [Required]
+        [MaxLength(200)]
         
         public string Information { get; set; }
 
         public string Preference { get; set; }
 
-       
+        [Required]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
         ErrorMessage = "Invalid email format")]
         public string EmployerEmail { get; set; }
--- HireManagement/HireManagement/Models/Worker.cs
+++ HireManagement/HireManagement/Models/Worker.cs
@@ -16,18 +16,22 @@ namespace HireManagement.Models
         [MaxLength(40)]
         public string LastName { get; set; }
         [Required]
-
+        [MaxLength(3)]
         public int Age { get; set; }
         [Required]
+        [MaxLength(100)]
         public string City { get; set; }
         [Required]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
         ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
-
+        [Required]
+        [MaxLength(15)]
         public string PhoneNumber { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string EmployedOnPosition { get; set; }
-
+        [Required]
         public DateTime JoinedDate { get; set; }
 
         public string UserID { get;set; }
